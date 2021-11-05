a861dba46057492b1c477ca69005312d1e61e604
mh-mehryar <44663511+mh-mehryar@users.noreply.github.com>
2021-11-05 19:14:59 +0330

    String Array

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Revers_Number_1
+{
+    class Program
+    {
+ static void Main(string[] args)
+        {
+
+            System.Console.WriteLine("Please Enter your Number :");
+            string n = Console.ReadLine();
+
+            string[] num = new string[n.Length];
+
+            for (var i = 0; i < n.Length; i++)
+            {
+                num[i] = n.Substring(i,1);
+            }
+            reversNum(num);
+            
+            
+        }
+        public static void reversNum(string[] number)
+        {
+            for (var i = number.Length - 1 ; i >= 0; i--)
+            {
+                System.Console.Write(number[i]);
+            }
+        
+
+        
+
+
+        }
+        }
+    }
